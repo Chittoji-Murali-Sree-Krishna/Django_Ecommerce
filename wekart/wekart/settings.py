79b703cf63b37e88d3691cb65482fce87b04e46b
ChittojiMuraliSreeKrishna <cmkrishna97@gmail.com>
2020-12-10 09:12:19 +0000

    Update settings.py

--- wekart/wekart/settings.py
+++ wekart/wekart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!+v++$)w(ce+6(l84qwxn)+*zv0b^_tlk!uuo@8rlsc06vpcs_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
